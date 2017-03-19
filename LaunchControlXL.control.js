58dfadfba099ae48913a95e1c2dfcfa51fc1a346
Robert Philp <robert.h.philp@gmail.com>
2017-03-19 22:05:30 +0000

    Added soft takeover volume faders

--- LaunchControlXL.control.js
+++ LaunchControlXL.control.js
@@ -8,6 +8,17 @@ host.defineController("Novation", "Launch Control XL", "0.1", "af3eb537-06ce-4f2
 host.defineMidiPorts(1, 1);
 // host.defineSysexIdentityReply('F0 7E 00 06 02 00 20 29 61 00 00 00 00 00 03 06 F7');
 
+var blink = 0;
+var blinkInterval;
+var transport;
+var tempo;
+var trackBank;
+
+function blinkTimer() {
+    blink = 1 - blink;
+    host.scheduleTask(blinkTimer, blinkInterval);
+}
+
 // We have a single row (1 scene) of 8 tracks represented by our 8 buttons on LaunchControl XL
 var NUM_TRACKS = 8;
 var NUM_SCENES = 1;
@@ -27,13 +38,13 @@ var BUTTONS_DIRECTIONAL = [BUTTON_UP, BUTTON_DOWN, BUTTON_LEFT, BUTTON_RIGHT];
 // Colour values that can be sent to the track focus/control buttons to represent some state
 var BLACK = 0;
 var YELLOW = 26;
-var LIGHT_GREEN = 25;
 var GREEN = 28;
-var RED = 3;
 
 // note values for track focus midi in/out
 var TRACK_FOCUS_BUTTONS = [41, 42, 43, 44, 57, 58, 59, 60];
 var TRACK_CONTROL_BUTTONS = [73, 74, 75, 76, 89, 90, 91, 92];
+
+var VOLUME_FADERS = [77, 78, 79, 80, 81, 82, 83, 84];
 
 /**
  * These represent one "row" of clips in the session view.
@@ -43,6 +54,17 @@ var TRACK_CONTROL_BUTTONS = [73, 74, 75, 76, 89, 90, 91, 92];
 var clipHasContent = initArray(0, NUM_TRACKS * NUM_SCENES);
 var clipIsPlaying = initArray(0, NUM_TRACKS * NUM_SCENES);
 var clipIsQueuedForPlayback = initArray(0, NUM_TRACKS * NUM_SCENES);
+var clipIsQueuedForStop = initArray(0, NUM_TRACKS * NUM_SCENES);
+
+// soft takeover store for volume faders
+var volume = initArray(0, NUM_TRACKS * NUM_SCENES);
+for(i=0; i<NUM_TRACKS; i++) {
+    volume[i] = {
+        changes: false,
+        jumps: false,
+        value: 0
+    }
+}
 
 // A function to create an indexed function for the Observers
 function getValueObserverFunc(index, targetVariable) {
@@ -51,15 +73,38 @@ function getValueObserverFunc(index, targetVariable) {
     }
 }
 
+// A function to create an indexed function for the Observers
+function getSoftTakeoverObserverFunc(index, targetVariable) {
+    return function(value) {
+        if (!targetVariable[index].changes) {
+            targetVariable[index].jumps = true
+        } else {
+            targetVariable[index].changes = false
+        }
+        targetVariable[index].value = value;
+    }
+}
+
 // Initialise - do all your setup in here!
 function init() {
+    transport = host.createTransport();
+    transport.tempo().value().addRawValueObserver(function(t) {
+        tempo = t;
+        blinkInterval = 1000 / tempo * 60 /4;
+    });
+    host.scheduleTask(blinkTimer, blinkInterval);
+
     trackBank = host.createTrackBank(NUM_TRACKS, 3, 1);
     for (var t = 0; t < NUM_TRACKS; t++)
     {
+        // clip observers
         trackBank.getChannel(t).clipLauncherSlotBank().setIndication(true);
         trackBank.getChannel(t).clipLauncherSlotBank().addHasContentObserver(getValueObserverFunc(t, clipHasContent));
         trackBank.getChannel(t).clipLauncherSlotBank().addIsPlayingObserver(getValueObserverFunc(t, clipIsPlaying));
         trackBank.getChannel(t).clipLauncherSlotBank().addIsPlaybackQueuedObserver(getValueObserverFunc(t, clipIsQueuedForPlayback));
+
+        // track volume observers
+        trackBank.getChannel(t).getVolume().modulatedValue().addValueObserver(128, getSoftTakeoverObserverFunc(t, volume))
     }
 
     // Callback for receiving MIDI input from our controller
@@ -71,9 +116,9 @@ function flush() {
     // This updates the "track focus" buttons representing the single row of clips in the session
     for (var i=0; i<NUM_TRACKS; i++) {
         colour = BLACK;
-        if (clipHasContent[i]) colour = YELLOW;
-        if (clipIsQueuedForPlayback[i]) colour = LIGHT_GREEN;
-        if (clipIsPlaying[i]) colour = GREEN;
+        if (clipHasContent[i] == 1) colour = YELLOW;
+        if (clipIsQueuedForPlayback[i] == 1) (blink == 1) ? colour = GREEN : colour = BLACK;
+        if (clipIsPlaying[i] == 1) colour = GREEN;
         host.getMidiOutPort(0).sendMidi(144, TRACK_FOCUS_BUTTONS[i], colour);
     }
 }
@@ -82,10 +127,9 @@ function flush() {
  * Respond to MIDI input from the controller.  At present, this branches out to other functions depending
  * on which sets of buttons are used. Just helps keep things modular.
  */
-
 function onMidi(status, data1, data2) {
     // Debugging call - dumps MIDI data into console
-    printMidi(status, data1, data2);
+    // printMidi(status, data1, data2);
 
     // Branches to other functions depending on input
     if (inArray(BUTTONS_DIRECTIONAL, data1)) {
@@ -96,6 +140,9 @@ function onMidi(status, data1, data2) {
     }
     if (inArray(TRACK_CONTROL_BUTTONS, data1)) {
         handleClipStop(status, data1, data2)
+    }
+    if (inArray(VOLUME_FADERS, data1)) {
+        handleVolumeFader(status, data1, data2);
     }
 }
 
@@ -122,6 +169,18 @@ function handleClipLaunch(status, data1, data2) {
 function handleClipStop(status, data1, data2) {
     track = TRACK_CONTROL_BUTTONS.indexOf(data1);
     if (track !== -1) trackBank.getChannel(track).clipLauncherSlotBank().stop();
+}
+
+function handleVolumeFader(status, data1, data2) {
+    track = VOLUME_FADERS.indexOf(data1);
+    if (track !== -1) {
+        var diff = data2 - volume[track].value;
+        if (!volume[track].jumps || (Math.abs(diff) < 2)) {
+            volume[track].changes = true;
+            volume[track].jumps = false;
+            trackBank.getChannel(track).getVolume().value().set(data2, 128);
+        }
+    }
 }
 
 function exit() {
